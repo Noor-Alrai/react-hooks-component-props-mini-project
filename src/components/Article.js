4cc9619419203afc3b28947591d86f07a72514c4
Noor Alrai <ameinnoor91@gmail.com>
2023-10-20 21:44:34 +0300

    Done

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Article({title , date, preview}){
+function Article({title , date, preview, minutes}){
  
   const articleDate = date || "January 1, 1970";
+  const coffeeCup =Math.ceil(minutes / 5);
+  const coffeeEmoji = '☕️'.repeat(coffeeCup);
+  const benBook = Math.ceil(minutes / 10);
+  const benbookEmoji = '🍱'.repeat(benBook);
+   const displayEmoj = minutes < 30 ? coffeeEmoji : benbookEmoji 
      return <article> 
               <h3>{title}</h3>
-              <small>{articleDate}</small>
+              <small>{articleDate}{displayEmoj} in read </small>
               <p>{preview}</p>
             </article>
 }
